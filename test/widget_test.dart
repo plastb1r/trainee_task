91af6d0772f7ff06874d071729eabeba9eccc9ab
plastb1r <www.kirill3101.ru@gmail.com>
2019-10-16 23:49:40 +0300

    подправил тест, чтобы не ругался

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:trainee_task/src/logInPage.dart';
+import 'package:trainee_task/src/log_in_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
